15778007447956bee51f77680d0f0b03f01ab1ca
Randomgamer22 <63390136+Randomgamer22@users.noreply.github.com>
2021-01-31 17:44:32 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import SignupLoginScreen from './screens/SignupLoginScreen';
+
+export default class App extends React.Component {
+  render() {
+    return (
+        <SignupLoginScreen/>
+    );
+  }
+}
+
